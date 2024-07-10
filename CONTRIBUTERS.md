f327b47354189f755fc261e3780b303d5f75dcbc
Rabjho <rabjho@protonmail.com>
2024-07-10 09:57:29 +0200

    Pivoted to use discord webhook instead of bot

--- CONTRIBUTERS.md
+++ CONTRIBUTERS.md
@@ -1,1 +1,3 @@
+[Rabjho](https://github.com/DenseOriginal)
+[Sibech](https://github.com/Sibech)
 [DenseOriginal](https://github.com/DenseOriginal)
--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 // Import fetch, client from discord.js and cheerio
-const { Client, GatewayIntentBits } = require("discord.js");
+const { WebhookClient } = require("discord.js");
 const cheerio = require("cheerio");
 const fs = require("fs");
 
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
+const webHookClient = new WebhookClient({
+  url: process.env.WEBHOOK_URL,
 });
 
 const getItems = async () => {
@@ -32,54 +28,42 @@ const getItems = async () => {
 
     return currentItems;
   } catch (error) {
-    console.error('woops');
+    console.error("woops");
     console.error(error);
     throw new Error("FAILED_TO_FETCH_ITEMS");
   }
 };
 
-const exit = () => {
-  client.destroy()
-    .finally(() => process.exit());
-}
+// Check for new items
+const currentItems = getItems();
 
-client.on("ready", async () => {
-  channel = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID);
+currentItems.then((currentItems) => {
+  const oldItems =
+    fs.existsSync("./items.json") &&
+    (result = fs.readFileSync("./items.json", "utf8"))
+      ? JSON.parse(result)
+      : [];
 
-  if (channel) {
-    // Check for new items
-    const currentItems = await getItems()
-  
-    // Import old items from items.json. If it doesn't exist, oldItems will be an empty array
-    const oldItems =
-      fs.existsSync("./items.json") &&
-        (result = fs.readFileSync("./items.json", "utf8"))
-        ? JSON.parse(result)
-        : [];
+  // Find the difference between the old and new items using filter/map on title, price and url parameters
+  const newItems = currentItems.filter(
+    (currentItem) =>
+      !oldItems.some(
+        (oldItem) =>
+          oldItem.title === currentItem.title &&
+          oldItem.price === currentItem.price &&
+          oldItem.url === currentItem.url
+      )
+  );
 
-    // Find the difference between the old and new items using filter/map on title, price and url parameters
-    const newItems = currentItems.filter(
-      (currentItem) =>
-        !oldItems.some(
-          (oldItem) =>
-            oldItem.title === currentItem.title &&
-            oldItem.price === currentItem.price &&
-            oldItem.url === currentItem.url
-        )
-    );
+  if (newItems.length !== 0) {
+    // Update items.json with new items
+    fs.writeFileSync("./items.json", JSON.stringify(currentItems, null, 2)); // TODO Replace with database for serverless?
 
-    if (newItems.length !== 0) {
-      // Update items.json with new items
-      fs.writeFileSync("./items.json", JSON.stringify(currentItems, null, 2)); // TODO Replace with database for serverless?
-
-      // Send message to channel with new items
-      await channel.send(
-        "@everyone Nye chips i kisten!" + "\n" + chipsJSONToString(newItems)
-      );
-    }
-
-    // Close bot when deon
-    exit();
+    // Send message to channel with new items
+    webHookClient.send({
+      content:
+        "@everyone Nye chips i kisten!" + "\n" + chipsJSONToString(newItems),
+    });
   }
 });
 
@@ -89,5 +73,3 @@ function chipsJSONToString(chips) {
     .map((chip) => `[${chip.title}](${chip.url}) - ${chip.price}`)
     .join("\n");
 }
-
-client.login(process.env.DISCORD_BOT_TOKEN);
