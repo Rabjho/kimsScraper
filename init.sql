1d8c29d3f06a665517e12ebb37ab9a8f1ca3731b
Kaszar127 <silas.b.christiansen@gmail.com>
2024-08-09 15:00:56 +0200

    Scrape info from product page

--- init.sql
+++ init.sql
@@ -15,16 +15,16 @@ CREATE TABLE types (
     FOREIGN KEY(item_id) REFERENCES items(item_id)
 );
 
-CREATE TABLE nutrionals (
+CREATE TABLE nutrition (
     item_id INT UNIQUE PRIMARY KEY,
-    calories INT NOT NULL,
-    fat DECIMAL(10, 2) NOT NULL,
-    saturated_fat DECIMAL(10, 2) NOT NULL,
-    carbohydrates DECIMAL(10, 2) NOT NULL,
-    sugar DECIMAL(10, 2) NOT NULL,
-    dietary_fiber DECIMAL(10, 2) NOT NULL,
-    protein DECIMAL(10, 2) NOT NULL,
-    salt DECIMAL(10, 2) NOT NULL,
-    ingredients VARCHAR(500) NOT NULL,
+    calories INT,
+    fat DECIMAL(10, 2),
+    saturated_fat DECIMAL(10, 2),
+    carbohydrates DECIMAL(10, 2),
+    sugar DECIMAL(10, 2),
+    dietary_fiber DECIMAL(10, 2),
+    protein DECIMAL(10, 2),
+    salt DECIMAL(10, 2),
+    ingredients VARCHAR(500),
     FOREIGN KEY(item_id) REFERENCES items(item_id)
 );
